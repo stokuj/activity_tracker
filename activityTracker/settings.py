98f03f1b42a8be8371febc97a784242a4c5b5b0e
Krystian Stasica <51967777+stokuj@users.noreply.github.com>
2023-01-16 13:16:12 +0100

    a

--- activityTracker/settings.py
+++ activityTracker/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+DEBUG = True
 if 'DYNO' in os.environ:
     print("Running on Heroku")
     
@@ -167,9 +167,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
